cd81890c1345dbd4f1aa6a880149dc673dd3bc8d
tekinosman <100043659+tekinosman@users.noreply.github.com>
2022-02-19 21:26:00 +0100

    Update db.py

--- helpers/db.py
+++ helpers/db.py
@@ -1,1 +1,12 @@
+import aioredis
 
+async def posts():
+	client = await aioredis.create_redis_pool(("127.0.0.1", 6379))
+	pipe = client.multi_exec()
+	pipe.hgetall("p:1", encoding = "utf-8")
+	pipe.hgetall("p:2", encoding = "utf-8")
+	pipe.hgetall("p:3", encoding = "utf-8")
+	data = await pipe.execute()
+	client.close()
+	await client.wait_closed()
+	return data
